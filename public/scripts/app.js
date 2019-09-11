1ff1a3ec6c7c4cf9f191dad1aed13e6ca510e3ea
cindyhalim <jcindyhalim@gmail.com>
2019-09-11 23:09:58 +0000

    updated validation error alerts with jQuery, fixed bugs, refactored code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//FUNCTIONS
+
 const renderTweets = (tweets) => {
+  console.log(tweets);
   if (Array.isArray(tweets)) {
     for (let item of tweets) {
       const $tweetElement = createTweetElement(item);
@@ -13,12 +16,11 @@ const renderTweets = (tweets) => {
   } else {
     $('#tweets-container').prepend(createTweetElement(tweets));
   }
-
 };
 
-//CREATE TWEET ELEMENT
+  //CREATE TWEET ELEMENT
 
-//escape function takes care of text-contents that might be mistakenly read as code
+//escape function takes care of cross-side scripting
 const escape = (str) => {
   const div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
@@ -29,7 +31,7 @@ const calculateDaysAgo = (date) => {
   let today = new Date();
   let timestampToday = today.getTime();
   return Math.floor(Math.abs((timestampToday - date) / (1000 * 60 * 60 * 24)));
-}
+};
 
 const createTweetElement = (data) => {
   return `<article class="tweet">
@@ -54,40 +56,72 @@ const createTweetElement = (data) => {
 </article>`;
 };
 
+const printError = message => {
+  const $error = $('.error');
+  $error.text(message);
+  $error.slideDown();
+};
 
-$('form').submit(function (event) {
-  event.preventDefault();
-  const text = $('textarea').val();
-  if (!text) {
-    alert('Please enter text');
-  } else if (text.length > 140) {
-    alert('Tweet is too long')
-  } else {
-    $.ajax('/tweets', { method: 'POST', data: $('.tweet-text').serialize() })
-    .then(addTweet());
-  }
-})
+//REQUESTS
 
+//fetching the latest tweet
+const addTweet = () => {
+  $.ajax({
+    url: '/tweets',
+    type: 'GET',
+    dataType: 'json'
+  }).then((response) => {
+    renderTweets(response[(response.length) - 1]);
+  });
+};
 
 //fetching tweets from '/tweets'
 const loadTweets = async () => {
   const response = await $.ajax({
     url: '/tweets',
-    type: 'GET'
+    type: 'GET',
+    dataType: 'json'
   });
   renderTweets(response);
-}
+};
+
+
+$('form').submit(function (event) {
+  event.preventDefault();
+  const text = $('textarea').val();
+
+  if (!text) {
+    printError("Error: tweet cannot be left blank");
+
+  } else if (text.length > 140) {
+    printError("Error: tweet cannot be more than 140 characters long");
+
+  } else {
+    if ($('p').hasClass('error')) {
+      $('.error').slideUp();
+    }
+    $.ajax('/tweets', { method: 'POST', data: $('.tweet-text').serialize() })
+      .then(() => addTweet());
+    $('textarea').val('');
+    $('textarea').keyup()
+  }
+});
 
 loadTweets();
 
-const addTweet = async () => {
-  const response = await $.ajax({
-    url: '/tweets',
-    type: 'GET'
-  });
-  renderTweets(response[response.length - 1]);
-}
-
+//SCROLL UP BUTTON
 $('.arrow').on('click', () => {
   $('.new-tweet').slideToggle();
 });
+
+$('#move-to-top-arrow').on('click', () => {
+  $('html, body').animate({ scrollTop: 0 }, 200);
+});
+
+$('window').scroll(() => {
+  $('#move-to-top-arrow').show();
+})
+
+
+
+
